f97f6b3cbc26ec0688648148e24b4068a097a6b5
Andy Maulana Yusuf <goingtoprofandy@gmail.com>
2019-08-09 02:14:23 +0700

    Add Form Dan List Laporan Pembukuan

---
+++ resources/views/laporan/form_laporan_pembukuan.blade.php
@@ -0,0 +1,14 @@
+<!-- Form Laporan Pembukuan -->
+<form id="form_laporan_pembukuan">
+    <div class="w3-row w3-margin-bottom w3-margin-top">
+        <label for="nama_laporan_pembukuan">Nama Laporan Pembukuan</label>
+        <select name="nama_laporan_pembukuan" id="nama_laporan_pembukuan" class="w3-input w3-border w3-round">
+        </select>
+    </div>
+    <div class="w3-row w3-margin-bottom">
+        <div class="w3-right">
+            <button class="w3-btn w3-red w3-round w3-border w3-border-red" type="reset">Cancel</button>
+            <button class="w3-btn w3-blue w3-round w3-border w3-border-blue" type="button">Simpan</button>
+        </div>
+    </div>
+</form>
--- resources/views/laporan/index.blade.php
+++ resources/views/laporan/index.blade.php
@@ -8,6 +8,24 @@
 <div class="w3-main" style="margin-left:300px;margin-top:43px;">
     @include ('templates.sitemap')
     <div class="w3-row w3-container">
+        <div class="w3-col m6 s12">
+            @component ('components.containers')
+                @slot ('container_title')
+                    Buat Laporan Pembukuan
+                @endslot
+                @include ('laporan.form_laporan_pembukuan')
+            @endcomponent
+        </div>
+        <div class="w3-col m6 s12">
+            @component ('components.containers')
+                @slot ('container_title')
+                    Semua Laporan Pembukuan
+                @endslot
+                @include ('laporan.table_laporan_pembukuan')
+            @endcomponent
+        </div>
+    </div>
+    <div class="w3-row w3-container">
         @component ('components.containers')
             @slot ('container_title')
                 Laporan
