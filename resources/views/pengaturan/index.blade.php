8d2e730de9f936558f4ce55f46ed746994a56089
Andy Maulana Yusuf <goingtoprofandy@gmail.com>
2019-08-09 21:33:36 +0700

    Add Settings

--- resources/views/pengaturan/index.blade.php
+++ resources/views/pengaturan/index.blade.php
@@ -6,7 +6,25 @@
 
 <!-- !PAGE CONTENT! -->
 <div class="w3-main" style="margin-left:300px;margin-top:43px;">
-
+    @include ('templates.sitemap')
+    <div class="w3-row w3-container">
+        <div class="w3-col m6 s12">
+            @component ('components.containers')
+                @slot ('container_title')
+                    Pengaturan Transaksi
+                @endslot
+                
+            @endcomponent
+        </div>
+        <div class="w3-col m6 s12">
+            @component ('components.containers')
+                @slot ('container_title')
+                    Pengaturan Backup Laporan
+                @endslot
+                
+            @endcomponent
+        </div>
+    </div>
 </div>
 {{-- Wrapper Content Stop --}}
 
