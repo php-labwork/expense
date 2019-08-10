5ff653930c305a87ae2c65e70c3ff6c8e5e27d3d
Andy Maulana Yusuf <goingtoprofandy@gmail.com>
2019-08-10 10:26:23 +0700

    Set Up Table List Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('avatar', 255);
+            $table->smallInteger('type');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2019_08_10_025638_table_master_data_kategori.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TableMasterDataKategori extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('m_kategori', function (Blueprint $table) {
+            $table->increments('id_kategori');
+            $table->smallInteger('id_jenis_transaksi')->unsigned();
+            $table->bigInteger('id_user')->unsigned();
+            $table->string('nama_kategori', 255);
+            $table->foreign('id_jenis_transaksi')->references('id_jenis_transaksi')->on('m_jenis_transaksi');
+            $table->foreign('id_user')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('m_kategori');
+    }
+}
